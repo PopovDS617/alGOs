3c0d6c3f3a45f158f38983e756bbe22a647d7c18
Popov Dmitry <popovds.bm@gmail.com>
2024-05-02 16:32:46 +0300

    add solution

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"algos/algorithms/search"
-)
-
 func main() {
 	//search.UseBinarySearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 17)
 	// sort.UseBubbleSort([]int{1024, 345, 44, 2, 25, 44, 1, 3, 101, 94})
@@ -21,6 +17,8 @@ func main() {
 	// fmt.Println("result is ", result)
 	// sort.UseInsertionSort([]int{100, 1, 90, 4}, true)
 	// sort.UseInsertionSort([]int{1024, 345, 44, 2, 25, 44, 1, 3, 101, 94}, false)
+	// search.UseDijkstraAlg()
+	// data := []int{98, 1, 3, 5, 6, 2, 22, 400, 100}
+	// res := sort.MergeSort(data)
 
-	search.UseDijkstraAlg()
 }
